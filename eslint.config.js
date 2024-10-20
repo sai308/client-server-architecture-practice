961d5bc7cd836318c26da040de2e6769a1233133
Andrew Sai <andrii.saienko@uzhnu.edu.ua>
2024-10-20 18:43:41 +0300

    🧑‍💻 (linter): eslint, prettier tweaked

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
-module.exports = require('neostandard')({
+const neostandard = require('neostandard');
+const { resolveIgnoresFromGitignore } = neostandard;
+
+module.exports = neostandard({
   noStyle: true, // Disable style rules to avoid conflicts with Prettier (default: false)
   semi: true, // Enable semicolons to avoid conflicts with Prettier (default: false)
-  ignores: ['node_modules'], // Ignore node_modules directory (default: []), replacement for .eslintignore
+  ignores: resolveIgnoresFromGitignore(), // Ignore node_modules directory (default: []), replacement for .eslintignore
 });
